f12ca0cdda7f81b1db15aac98219641befa538a2
raoni157 <raoni.itar@gmail.com>
2021-08-20 18:11:53 -0300

    polimento

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  secret: 'akasdfj0út23453456+54qt23qvqwfqwerqwerqewrasdasdasdaa6()',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
